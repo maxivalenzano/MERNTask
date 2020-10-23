9a305ccb4e652941744d7177ca57ebe73e36b913
maxivalenzano <maxivalenzano@gmail.com>
2020-10-22 23:57:06 -0300

    separando el sidebar

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return ( 
+        <aside>
+            <h1>MERN<spam>Tasks</spam></h1>
+            <div className="proyectos">
+                <h2>Tus proyectos</h2>
+            </div>
+        </aside>
+     );
+}
+ 
+export default Sidebar;
